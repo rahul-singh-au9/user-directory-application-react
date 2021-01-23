725556bbbda4afa984ef24ba904072de2799bb76
rahul-singh-au9 <rahulsg1508@gmail.com>
2021-01-23 12:49:53 +0530

    user-directory-application-react

--- user-directory-application/src/App.js
+++ user-directory-application/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter,Route} from "react-router-dom";
+import {BrowserRouter,Route, Switch} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Navbar from "./components/layout/Navbar";
 import AddUser from "./components/users/AddUser";
@@ -9,10 +9,12 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/users/add" component={AddUser}/>
-      <Route exact path="/users/edit/:id" component={EditUser} />
-      <Route exact path="/users/:id" component={User} />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/users/add" component={AddUser}/>
+        <Route exact path="/users/edit/:id" component={EditUser} />
+        <Route exact path="/users/:id" component={User} />
+      </Switch>
     </BrowserRouter>
   );
 }
